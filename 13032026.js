35b4a917cbf7cff5d5a7853cb76ca326096a7fc5
João Paulo <j.sasdelli@escola.pr.gov.br>
2026-03-19 07:44:32 -0300

    create : Aula //19032026

--- 13032026.js
+++ 13032026.js
@@ -18,3 +18,23 @@ function coiso(lucro1,lucro2){
 }
 
 console.log(coiso(lucro1,lucro2))
+
+function calculadora(ari,a,b){
+  switch(ari){
+    case '+':
+      reesultado = a + b;
+      break;
+    case '-':
+      resultado = a - b;
+      break;
+    case '*':
+      resultado = a * b;
+      break;
+    case '/':
+      resultado = a / b;
+      break;
+    default;
+      resultado = 'Operador inválido';  
+  }
+  return resultado
+}
