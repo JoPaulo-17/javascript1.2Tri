33f65e82ec126244b5f91dc7f010d4e8eaf4943e
João Paulo <j.sasdelli@escola.pr.gov.br>
2026-03-27 07:56:26 -0300

    update

--- 27032026.js
+++ 27032026.js
@@ -1,3 +1,10 @@
 const filanumber - [1, 2, 3, 4, 5, 6];
 
 const filastringanimgos = ["UnaPotato", "Café", "Rogerio Aldegar"];
+
+function lerAmigos(fileStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length} amigos`)
+    for(let i = 0; i < filaStringAmigos.length;i++){
+        console.log(`UnaPotato ${i}`)
+    }
+}
